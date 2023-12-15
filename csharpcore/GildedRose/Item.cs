b4bd517313170776cae4f744da61526d74964f58
BecaLParker <beca.parker@red-gate.com>
2023-12-15 16:57:13 +0000

    Make Item.Name immutable

--- csharpcore/GildedRose/Item.cs
+++ csharpcore/GildedRose/Item.cs
@@ -2,7 +2,7 @@
 
 public class Item
 {
-    public string Name { get; set; }
+    public string Name { get; init; }
     public int SellIn { get; set; } //denotes the number of days we have to sell the item
     public int Quality { get; set; } //denotes how valuable the item is
 }
