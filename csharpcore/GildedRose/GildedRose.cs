03d930772dcb39b162ca75d8fccced3f919ac2c4
BecaLParker <beca.parker@red-gate.com>
2023-12-15 16:53:06 +0000

    Convert to primary constructor

--- csharpcore/GildedRose/GildedRose.cs
+++ csharpcore/GildedRose/GildedRose.cs
@@ -2,85 +2,78 @@
 
 namespace GildedRose;
 
-public class GildedRose
+public class GildedRose(IList<Item> items)
 {
-    private readonly IList<Item> _items;
-
-    public GildedRose(IList<Item> items)
-    {
-        _items = items;
-    }
-
     public void UpdateQuality()
     {
-        for (var i = 0; i < _items.Count; i++)
+        for (var i = 0; i < items.Count; i++)
         {
-            if (_items[i].Name != "Aged Brie" && _items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+            if (items[i].Name != "Aged Brie" && items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
             {
-                if (_items[i].Quality > 0)
+                if (items[i].Quality > 0)
                 {
-                    if (_items[i].Name != "Sulfuras, Hand of Ragnaros")
+                    if (items[i].Name != "Sulfuras, Hand of Ragnaros")
                     {
-                        _items[i].Quality = _items[i].Quality - 1;
+                        items[i].Quality = items[i].Quality - 1;
                     }
                 }
             }
             else
             {
-                if (_items[i].Quality < 50)
+                if (items[i].Quality < 50)
                 {
-                    _items[i].Quality = _items[i].Quality + 1;
+                    items[i].Quality = items[i].Quality + 1;
 
-                    if (_items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                    if (items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        if (_items[i].SellIn < 11)
+                        if (items[i].SellIn < 11)
                         {
-                            if (_items[i].Quality < 50)
+                            if (items[i].Quality < 50)
                             {
-                                _items[i].Quality = _items[i].Quality + 1;
+                                items[i].Quality = items[i].Quality + 1;
                             }
                         }
 
-                        if (_items[i].SellIn < 6)
+                        if (items[i].SellIn < 6)
                         {
-                            if (_items[i].Quality < 50)
+                            if (items[i].Quality < 50)
                             {
-                                _items[i].Quality = _items[i].Quality + 1;
+                                items[i].Quality = items[i].Quality + 1;
                             }
                         }
                     }
                 }
             }
 
-            if (_items[i].Name != "Sulfuras, Hand of Ragnaros")
+            if (items[i].Name != "Sulfuras, Hand of Ragnaros")
             {
-                _items[i].SellIn = _items[i].SellIn - 1;
+                items[i].SellIn = items[i].SellIn - 1;
             }
 
-            if (_items[i].SellIn < 0)
+            if (items[i].SellIn < 0)
             {
-                if (_items[i].Name != "Aged Brie")
+                if (items[i].Name != "Aged Brie")
                 {
-                    if (_items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                    if (items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        if (_items[i].Quality > 0)
+                        if (items[i].Quality > 0)
                         {
-                            if (_items[i].Name != "Sulfuras, Hand of Ragnaros")
+                            if (items[i].Name != "Sulfuras, Hand of Ragnaros")
                             {
-                                _items[i].Quality = _items[i].Quality - 1;
+                                items[i].Quality = items[i].Quality - 1;
                             }
                         }
                     }
                     else
                     {
-                        _items[i].Quality = _items[i].Quality - _items[i].Quality;
+                        items[i].Quality = items[i].Quality - items[i].Quality;
                     }
                 }
                 else
                 {
-                    if (_items[i].Quality < 50)
+                    if (items[i].Quality < 50)
                     {
-                        _items[i].Quality = _items[i].Quality + 1;
+                        items[i].Quality = items[i].Quality + 1;
                     }
                 }
             }
