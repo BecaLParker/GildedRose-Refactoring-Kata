645c97f0ba18733877854a8733db5ba06b1b0dc3
BecaLParker <beca.parker@red-gate.com>
2023-12-15 16:07:00 +0000

    Add dev notes based on GildedRoseRequirements.md

--- csharpcore/GildedRose/Item.cs
+++ csharpcore/GildedRose/Item.cs
@@ -3,6 +3,6 @@
 public class Item
 {
     public string Name { get; set; }
-    public int SellIn { get; set; }
-    public int Quality { get; set; }
+    public int SellIn { get; set; } //denotes the number of days we have to sell the item
+    public int Quality { get; set; } //denotes how valuable the item is
 }
--- csharpcore/GildedRoseTests/GildedRoseTest.cs
+++ csharpcore/GildedRoseTests/GildedRoseTest.cs
@@ -14,4 +14,20 @@ public class GildedRoseTest
         app.UpdateQuality();
         Assert.AreEqual("fixme", items[0].Name);
     }
+    
+    //Existing behvaiour to pin based on GildedRoseRequirements.md
+    //TODO: Test that Quality and sellIn are reduced on each item after one day
+    //TODO: Test that Quality degrades twice as fast once the sell by date has passed
+    //TODO: Test that Quality of an item is never negative
+    //TODO: Test that Aged Brie increases in Quality the older it gets
+    //TODO: Test that Quality of an item is never more than 50 (except Sulfuras, which is a constant 80
+    //TODO: Test that Sulfuras (a legendary item) never has to be sold or decreases in Quality
+    //TODO: Test that Backstage passes increases in Quality as its SellIn value approaches
+    //TODO: Test that Backstage passes increases in Quality by 2 when there are 10 SellIn days or less
+    //TODO: Test that Backstage passes increases in Quality by 3 when there are 5 SellIn days or less
+    //TODO: Test that Backstage passes drops to 0 in Quality after the concert
+    
+    //New behaviour to add
+    //TODO: Test that Conjured items degrade in Quality twice as fast as normal items
+    
 }
